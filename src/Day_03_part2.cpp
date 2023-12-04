47646657e6487dd535e528afc372ba9a569f2cee
Álvaro Borrás <alvaroborrasf@gmail.com>
2023-12-04 14:50:37 +0100

    Add solution Day 3, part 2

---
+++ src/Day_03_part2.cpp
@@ -0,0 +1,72 @@
+//
+// Created by Álvaro Borrás on 04/12/23.
+//
+
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+
+const std::vector<int> dx = {0, 0, 1, 1, 1, -1, -1, -1};
+const std::vector<int> dy = {1, -1, 1, 0, -1, 1, 0, -1};
+
+int main() {
+  std::ifstream input_file("../input/input_day_03_part1.txt");
+  std::string line;
+
+  long long answer = 0;
+  if (input_file.is_open()) {
+    std::vector<std::string> v;
+    while (std::getline(input_file, line)) { v.push_back(line); }
+    input_file.close();
+
+    int nLines = (int)v.size();
+    int nCols = (int)v[0].size();
+
+    std::map<std::pair<int, int>, std::vector<int>> count;
+
+    for (int i = 0; i < nLines; ++i) {
+      long long curNumber = 0;
+      std::set<std::pair<int, int>> gears;
+      for (int j = 0; j < nCols; ++j) {
+        if ('0' <= v[i][j] && v[i][j] <= '9') {
+          curNumber = 10 * curNumber + (int)(v[i][j] - '0');
+
+          for (int neigh = 0; neigh < 8; ++neigh) {
+            int n_x = i + dx[neigh];
+            int n_y = j + dy[neigh];
+
+            if (n_x < 0 || n_x >= nLines) continue;
+            if (n_y < 0 || n_y >= nCols) continue;
+
+            if (v[n_x][n_y] == '*') { gears.insert({n_x, n_y}); }
+          }
+        }
+
+        if (j == nCols - 1 || !isdigit(v[i][j])) {
+          if (curNumber) {
+            for (auto& [gx, gy] : gears) { count[{gx, gy}].push_back(curNumber); }
+            gears = {};
+          }
+          curNumber = 0;
+        }
+      }
+    }
+
+    for (auto& [it, v] : count) {
+      std::cout << it.first << " " << it.second << std::endl;
+      for (auto x : v) { std::cout << x << " "; }
+      std::cout << std::endl;
+      if (v.size() == 2) { answer += std::accumulate(v.begin(), v.end(), 1, std::multiplies<int>()); }
+    }
+
+    std::cout << answer << std::endl;
+  } else {
+    std::cout << "Unable to open file";
+  }
+
+  return 0;
+}
